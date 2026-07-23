b7f9d3c6b0ede890f3584075fe8a6c9cd8a1e487
sufiyanoa <sufiyanomerali00@gmai.com>
2026-07-24 01:47:40 +0530

    small change in styling

--- habit-flow-fr/src/components/stats.jsx
+++ habit-flow-fr/src/components/stats.jsx
@@ -1,6 +1,6 @@
 const Stats = ({ completed, total, pending }) => {
   return (
-    <div className="flex justify-around">
+    <div className="flex justify-around mb-3">
       <p className="bg-white border px-1 rounded">Total: {total}</p>
       <p className="bg-white border px-1 rounded">Pending: {pending} </p>
       <p className="bg-white border px-1 rounded">Completed: {completed} </p>
